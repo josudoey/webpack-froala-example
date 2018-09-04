4d60ced43039ce14bb04842a2cddbd8fec93b9c3
Joey Fang <josudoey@gmail.com>
2018-09-04 18:52:58 +0800

    [why] fix bug

--- app/assets/froala-editor/index.js
+++ app/assets/froala-editor/index.js
@@ -4,7 +4,6 @@ require('froala-editor/css/froala_editor.css')
 require('froala-editor/css/froala_style.css')
 require('froala-editor/js/froala_editor.min.js')
 
-
 require('froala-editor/js/plugins/align.min.js')
 require('froala-editor/js/plugins/code_beautifier.min.js')
 require('froala-editor/js/plugins/draggable.min.js')
@@ -20,7 +19,7 @@ require('froala-editor/js/plugins/inline_style.min.js')
 require('froala-editor/js/plugins/save.min.js')
 
 require('froala-editor/css/plugins/code_view.css')
-equire('froala-editor/js/plugins/code_view.min.js')
+require('froala-editor/js/plugins/code_view.min.js')
 
 require('froala-editor/css/plugins/colors.css')
 require('froala-editor/js/plugins/colors.min.js')
@@ -28,23 +27,20 @@ require('froala-editor/js/plugins/colors.min.js')
 require('froala-editor/css/plugins/emoticons.css')
 require('froala-editor/js/plugins/emoticons.min.js')
 
+require('froala-editor/css/plugins/image.css')
+require('froala-editor/js/plugins/image.min.js')
 
 require('froala-editor/css/plugins/image_manager.css')
 require('froala-editor/js/plugins/image_manager.min.js')
 
-require('froala-editor/css/plugins/image.css')
-require('froala-editor/js/plugins/image.min.js')
-
 require('froala-editor/css/plugins/line_breaker.css')
 require('froala-editor/js/plugins/line_breaker.min.js')
-
 
 require('froala-editor/css/plugins/quick_insert.css')
 require('froala-editor/js/plugins/quick_insert.min.js')
 
 require('froala-editor/css/plugins/table.css')
 require('froala-editor/js/plugins/table.min.js')
-
 
 require('froala-editor/css/plugins/file.css')
 require('froala-editor/js/plugins/file.min.js')
@@ -55,7 +51,7 @@ require('froala-editor/css/plugins/video.css')
 require('froala-editor/js/plugins/video.min.js')
 
 require('froala-editor/css/plugins/emoticons.css')
-rrequire('froala-editor/js/plugins/emoticons.min.js')
+require('froala-editor/js/plugins/emoticons.min.js')
 
 require('froala-editor/css/plugins/fullscreen.css')
 require('froala-editor/js/plugins/fullscreen.min.js')
